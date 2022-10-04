d20d041b1da5d2a1b4359889450dcbf1c3b38313
uncogito <ulli.rozen@gmail.com>
2022-10-04 11:52:10 +0200

    navbaractiveclass
    a bit of refactor, a tiny bit of css, mostly getting the menu to work.

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
 # Landing Page Project
 
-## Table of Contents
-
-* [Instructions](#instructions)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+Adding functionality to a reactive blog, including marking active sections, dynamic building of navigation menu, scrolling
+based on Javascript and not href pointers.
--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ function scrollOnClick() {
         e.preventDefault();
         sections.forEach(section => {
             if (section.dataset.nav === e.target.textContent) {
+                /*e.target.classList.add("your-active-class");*/
                 section.scrollIntoView({
                     behavior: "smooth"
                 })
@@ -37,15 +38,21 @@ function displaySubmittedMessage(e) {
     alert("Submission Succesfull");
 }
 
-/* sets active section according to user scrolling*/
+/* sets active section according to user scrolling and active nav item*/
 function activateSectionInViewport() {
     window.addEventListener('scroll', () => {
-        const sections = document.querySelectorAll('section');
-
+        const navLinks = navBarList.querySelectorAll('li > a');
         function checkIfActive(section) {
             const sectionLocation = section.getBoundingClientRect();
             if (sectionLocation.top < window.innerHeight * 0.5 && sectionLocation.bottom > window.innerHeight * 0.5) {
                 section.classList.add("your-active-class");
+                for (let i=0; i<navLinks.length; i++ ) {
+                    if (navLinks[i].innerHTML === section.dataset.nav) {
+                        navLinks[i].classList.add("your-active-class")
+                    } else {
+                        navLinks[i].classList.remove("your-active-class")
+                    }
+                }
             } else {
                 section.classList.remove("your-active-class");
             }
